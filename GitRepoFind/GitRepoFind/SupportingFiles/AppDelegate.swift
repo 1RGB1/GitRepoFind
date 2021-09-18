0480c5376b6d9ed0ad4b345bf51bbabcb9109ff1
Ahmad Ragab <a.ragab2012@gmail.com>
2021-09-18 23:00:27 +0200

    Organizing files and folders

---
+++ GitRepoFind/GitRepoFind/SupportingFiles/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  GitRepoFind
+//
+//  Created by Ahmad Ragab on 18/09/2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
