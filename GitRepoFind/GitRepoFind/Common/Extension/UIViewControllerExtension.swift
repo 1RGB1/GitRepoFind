87c200e3bf3d92236b1718bc8b116cdfaa3d24a6
Ahmad Ragab <a.ragab2012@gmail.com>
2021-09-20 20:49:22 +0200

    Remove progress hud configuration to view controller extension

--- GitRepoFind/GitRepoFind/Common/Extension/UIViewControllerExtension.swift
+++ GitRepoFind/GitRepoFind/Common/Extension/UIViewControllerExtension.swift
@@ -7,10 +7,29 @@
 
 import Foundation
 import SafariServices
+import ProgressHUD
 
 extension UIViewController {
+    
     func presentSFSafariViewControllerFor(url: URL) {
         let safariViewcontroller = SFSafariViewController(url: url)
         present(safariViewcontroller, animated: true, completion: nil)
+    }
+    
+    func configureProgress() {
+        ProgressHUD.animationType = .lineScaling
+        ProgressHUD.colorHUD = .systemGray
+        ProgressHUD.colorBackground = .lightGray
+        ProgressHUD.colorAnimation = .systemBlue
+        ProgressHUD.colorProgress = .systemBlue
+        ProgressHUD.fontStatus = UIFont(name: "HelveticaNeue-Regular", size: 18) ?? .boldSystemFont(ofSize: 18)
+        
+        let defaultImageConfiguration = UIImage.SymbolConfiguration(scale: .large)
+        if let successImage = UIImage(systemName: "checkmark.circle", withConfiguration: defaultImageConfiguration) {
+            ProgressHUD.imageSuccess = successImage
+        }
+        if let faildImage = UIImage(systemName: "xmark.octagon", withConfiguration: defaultImageConfiguration) {
+            ProgressHUD.imageError = faildImage
+        }
     }
 }
